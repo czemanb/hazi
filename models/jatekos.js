84ac5e419b241fdffa9009f80d5644d009322265
czemanb <48368776+czemanb@users.noreply.github.com>
2020-12-12 11:27:24 +0100

    Update jatekos.js

--- models/jatekos.js
+++ models/jatekos.js
@@ -1,7 +1,7 @@
 const Schema = require('mongoose').Schema;
 const db = require('../config/database');
 
-const Jatekos = db.model('Jatekos', {
+const Jatekos = db.model('Jatekos',{
     nev: String,
     mezszam: Number,
     teljesitmeny: String,
